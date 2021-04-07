3a685e841fb6ba6120cfe093d3007a98ea0cd756
thakurzaildarrana <thakurzaildarrana@gmail.com>
2021-04-07 16:52:54 -0700

    clean up

--- BankingAPPGroup7/src/main/java/com/group7/bankingapp/constants/GeneralConstants.java
+++ BankingAPPGroup7/src/main/java/com/group7/bankingapp/constants/GeneralConstants.java
@@ -2,9 +2,7 @@ package com.group7.bankingapp.constants;
 
 public class GeneralConstants {
 
-    public static final String ROLE_PREFFIX = "ROLE_";
-    public static final String ROLE_ADMIN = ROLE_PREFFIX + "ADMIN";
-    public static final String ROLE_USER = ROLE_PREFFIX + "USER";
+
 
 
 
